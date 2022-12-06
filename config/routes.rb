0c8c3718ade044144e5c0a4abba41ffde05e444a
vicente gabriel novella corchado <gabriel.novella@outlook.com>
2022-12-06 15:06:34 +0100

    added movies

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "lists#index"
   resources :lists do
-    resources :pages, only: [:update, :destroy]
+    resources :pages, only: [:edit, :update, :destroy]
+  end
+
+  resources :lists do
+    resources :bookmarks
   end
   get "dashboard", to: "pages#dashboard"
 end
